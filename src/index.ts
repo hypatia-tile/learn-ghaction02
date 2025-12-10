e0f6cdbf2e34d7009626c78d4fe7444ed0efd352
shinokun <hypatia.tile02021@gmail.com>
2025-12-11 00:28:11 +0900

    Add simple node project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// my-script.ts
+function greet(name: string) {
+  console.log(`Hello, ${name}!`);
+}
+
+const args = process.argv.slice(2); // Slice to remove 'node' and the script path
+if (args.length > 0 && args[0] !== undefined) {
+  greet(args[0]);
+} else {
+  console.log("Please provide a name as an argument.");
+}
